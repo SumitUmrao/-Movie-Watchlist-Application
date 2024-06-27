385a833503ff978819c5b96980ac85abb44dc8ca
Sumit Umrao <129044589+SumitUmrao@users.noreply.github.com>
2024-06-27 21:44:12 +0530

    Add files via upload

---
+++ api-data.js
@@ -0,0 +1,5 @@
+let ts ="1693124985291";
+let publicKey = "0067bd86d85c79f86e656624bf6a963d";
+let hashVal = "cd54bc54c38db8b2b7e62f7d9484d225";
+
+//private key ="92e45c713abd04183f5be3ee3cacaa5a8eeda61e"
